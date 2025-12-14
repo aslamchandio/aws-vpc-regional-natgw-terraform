bf252b364471bf57fd23b3e238e6ff4dbe942634
aslam.chandio <aslam.chandio03@gmail.com>
2025-12-14 17:58:27 +0500

    second commit change code

--- c1-02-remote-backend.tf
+++ c1-02-remote-backend.tf
@@ -1,8 +1,8 @@
 terraform {
   # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket  = "aslam-terraform-bucket"
-    key     = "prod/tf-vpc-state-file/terraform.tfstate"
+    bucket  = "aws-bucket"
+    key     = "prod/state-file/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
 
